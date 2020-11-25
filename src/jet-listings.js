ada7d6cc2ed7aaa04c35e88b571f649c94b17b15
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2020-11-25 08:46:09 +0100

    AERO-6 | Corrected the jet listings page UI
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/jet-listings.js
+++ src/jet-listings.js
@@ -1,19 +1,54 @@
 /* eslint-disable max-lines-per-function */
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import VehicleBooking from './components/layout/vehicle-booking';
+// import { useLocation } from 'react-router-dom';
+import styled from '@emotion/styled';
+import Body from './components/layout/body';
+import Section from './components/section';
+import JetCard from './components/jet-card';
+import Logo from './images/logo.png';
+
+const JetListingsWrapper = styled.div`
+
+  > section {
+    padding-top: 8rem;
+    padding-bottom: 10rem;
+  }
+`;
+
+const JetCardWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: -2.5%;
+  flex-wrap: wrap;
+  margin-right: -2.5%;
+`;
 
 const JetListings = () => {
-  const location = useLocation();
-  const { jetBookingData } = location.state;
-  console.log('jet booking data ', jetBookingData);
-  const { jets } = jetBookingData;
+  // const location = useLocation();
+  // const { jets } = location.state;
+
+  const jets = [2, 3, 4, 5, 6, 7, 56, 564, 445, 5656, 3434, 4525, 25];
 
   return (
-    <VehicleBooking
-      bookingData={jetBookingData}
-      vehicles={jets}
-    />
+    <Body>
+      <JetListingsWrapper>
+        <Section>
+          <h1>Avaliable Jets</h1>
+          <JetCardWrapper>
+            {
+              jets.map((jet) => (
+                <JetCard
+                  key={jet}
+                  name="Hilux"
+                  image={Logo}
+                  description="I am a jet"
+                />
+              ))
+            }
+          </JetCardWrapper>
+        </Section>
+      </JetListingsWrapper>
+    </Body>
   );
 };
 
